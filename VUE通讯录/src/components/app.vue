b1d3bcf13de2a437ca0d896f4b75644099e67814
Zhaozhihao1997-IU <74164763+Zhaozhihao1997-IU@users.noreply.github.com>
2020-11-09 11:13:43 +0800

    Add files via upload

---
+++ VUE通讯录/src/components/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="contain">  
+        <h1>通 义 达</h1>
+        <h2>西安通义达电子科技服务有限公司</h2>
+        <div class="btns">
+            <router-link to="/home/register" class="btn">注册</router-link>
+            <router-link to="/home/login">登录</router-link>
+        </div>
+        <router-view></router-view> 
+    </div>
+</template>  
+
+<style scoped>
+@import '../assets/css/app.css'
+</style>
